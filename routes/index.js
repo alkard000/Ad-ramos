ce2c323bcd915121332f371269027aa7a9c620e2
Alkard000 <aburtoivan620@gmail.com>
2019-08-16 18:42:49 -0400

    Agregando los controladores del proyecto

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
+//importar controlador
+const proyectosController = require('../controllers/proyectosController');
 module.exports = function(){
     //rutas para el home
-    router.get('/', (req, res) => {
-        res.send('Index');
-    });
-    router.get('/nosotros', (req, res) => {
-        res.send('Nosotros');
-    });
+    router.get('/', proyectosController.proyectosHome);
+    router.get('/nosotros', proyectosController.nosotros);
     return router;
 }
 
