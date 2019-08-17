806edef8cd10a651237464f25e3c3cbab318d53b
Alkard000 <aburtoivan620@gmail.com>
2019-08-17 04:52:23 -0400

    Agregando el formulario

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -3,3 +3,11 @@ exports.proyectosHome = (req, res) => {
         nombrePagina : 'Ramos'
     });
 }
+exports.formularioRamo = (req, res) => {
+    res.render('nuevoRamo', {
+        nombrePagina : 'Nuevo Ramo'
+    });
+}
+exports.nuevoRamo = (req, res) => {
+    res.send('Enviaste el Formulario')
+}
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const proyectosController = require('../controllers/proyectosController');
 module.exports = function(){
     //rutas para el home
     router.get('/', proyectosController.proyectosHome);
+    router.get('/nuevo-ramo', proyectosController.formularioRamo)
+    router.post('/nuevo-ramo', proyectosController.nuevoRamo)
     return router;
 }
 
